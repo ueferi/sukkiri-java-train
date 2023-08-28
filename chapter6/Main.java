5293a19f1d21a491b0e145c220bcc20719653473
ueferi <58869038+ueferi@users.noreply.github.com>
2023-08-28 15:38:08 +0900

    docs: ✨ ファイルを追加する

---
+++ chapter6/Main.java
@@ -0,0 +1,14 @@
+package chapter6;
+
+import chapter6.comment.Zenhan;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    chapter6.comment.Kouhan.callDeae();
+    chapter6.comment.Kouhan.showMondokoro();
+  }
+
+}
---
+++ chapter6/comment/Kouhan.java
@@ -0,0 +1,15 @@
+package chapter6.comment;
+
+public class Kouhan {
+  public static void callDeae() {
+    System.out.println("ええい、こしゃくな。くせ者だ！であえい！");
+  }
+
+  public static void showMondokoro() throws Exception {
+    System.out.println("飛車さん、角さん。もういいでしょう。");
+    System.out.println("この紋所が目に入らぬか！");
+    Thread.sleep(3000);// 待ち時間3秒を設定
+    Zenhan.doTogame();// もう一度、とがめる
+  }
+
+}
