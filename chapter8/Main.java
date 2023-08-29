f10ffa998a1703d37cc847eda68c9f3af692f411
ueferi <58869038+ueferi@users.noreply.github.com>
2023-08-29 15:56:36 +0900

    docs: 📚 ファイルの更新をする

--- chapter8/Main.java
+++ chapter8/Main.java
@@ -8,11 +8,19 @@ public class Main {
     // 2.フィールドに初期値をセット
     h.name = "ミナト";
     h.hp = 100;
-    System.out.println("勇者" + h.name + "を生み出しました！");
-    // 3.勇者のメソッドを呼び出しておく
-    h.sit(5);
+
+    // お化けキノコAとBを呼び出す
+    Matango m1 = new Matango();
+    m1.hp = 50;
+    m1.suffix = 'A';
+    Matango m2 = new Matango();
+    m2.hp = 48;
+    m2.suffix = 'B';
+
+    // 3.冒険のはじまり
     h.slip();
-    h.sit(25);
+    m1.run();
+    m2.run();
     h.run();
   }
 
