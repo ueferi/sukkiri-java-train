94313b58e36ddf8fc74a2ce58a2c0d52c9815c21
ueferi <58869038+ueferi@users.noreply.github.com>
2023-09-04 15:42:45 +0900

    docs: ✨ ファイルを追加する

---
+++ chapter11/CleaningService.java
@@ -0,0 +1,9 @@
+package chapter11;
+
+public interface CleaningService {
+  Shirt washShirt(Shirt s);
+
+  Towl washTowl(Towl t);
+
+  Coat washCoat(Coat c);
+}
---
+++ chapter11/practice/Thing.java
@@ -0,0 +1,9 @@
+package chapter11.practice;
+
+public interface Thing {
+  double getWeight();
+
+  void setWeight(double weight);
+  // double型の重さを設定するだけなので戻り値のないvoid
+
+}
